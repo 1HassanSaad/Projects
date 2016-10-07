ecd25cf5f9f2308eef6e0e4344e9e5c296a91055
Hassan Saad <1hassansaad1@gmail.com>
2016-10-07 20:32:07 +0300

    Update Search_Engine_Crawler.py

--- Search_Engine_Crawler.py
+++ Search_Engine_Crawler.py
@@ -1,11 +1,46 @@
+#Finishing the page ranking algorithm.
+
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + ( d * ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+
+def crawl_web(seed): # returns index, graph of inlinks
+    tocrawl = [seed]
+    crawled = []
+    graph = {}  # <url>, [list of pages it links to]
+    index = {} 
+    while tocrawl: 
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            union(tocrawl, outlinks)
+            crawled.append(page)
+    return index, graph
+
 def get_page(url):
     try:
-        import urllib.request
-        return str(urllib.request.urlopen(url).read())
+        import urllib
+        return str(urllib.urlopen(url).read())
     except:
         return "error"
-
-
+    
 def get_next_target(page):
     start_link = page.find('<a href=')
     if start_link == -1: 
@@ -15,98 +50,41 @@ def get_next_target(page):
     url = page[start_quote + 1:end_quote]
     return url, end_quote
 
-
-def get_all_links(page): 
-    links = [] 
-    while True: 
+def get_all_links(page):
+    links = []
+    while True:
         url, endpos = get_next_target(page)
         if url:
             links.append(url)
-            page = page[endpos:] 
-        else: 
-            break 
+            page = page[endpos:]
+        else:
+            break
     return links
 
 
-def union(p,q):
-    for e in q:
-        if e not in p:
-            p.append(e)
+def union(a, b):
+    for e in b:
+        if e not in a:
+            a.append(e)
 
-def crawl_web(seed): #1 
-    tocrawl = [seed] 
-    crawled = [] 
-    index = [] 
-    while tocrawl: 
-        page = tocrawl.pop()
-        if page not in crawled: 
-            content = get_page(page) 
-            add_page_to_index(index, page, content) 
-            union(tocrawl, get_all_links(content)) 
-            crawled.append(page) 
-    return index 
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+        
+def add_to_index(index, keyword, url):
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]
 
-def add_page_to_index(index, url, content): 
-    words = content.split() 
-    for word in words: 
-        add_to_index(index, word, url) 
+def lookup(index, keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
 
-def add_to_index(index, keyword, url): 
-    for entry in index: 
-        if entry[0] == keyword: 
-            entry[1].append(url) 
-            return 
-    # not found, add new keyword to index 
-    index.append([keyword, [url]]) 
-
-def lookup(index, keyword): 
-    s=0
-    for e in keyword:
-	    s+=ord(e)
-    bucket = s % len(index)
-    l = index[bucket]
-    for entry in l : 
-        if entry[0] == keyword: 
-            return entry[1] 
-    return None
-
-def empty_buckets(size):
-    i=0
-    table=[]
-    while i<size:
-        table.append([])
-        i=i+1
-    return table
-
-def add_to_table(result,size_of_table):
-    table = empty_buckets(size_of_table)
-    for list in result:
-        s=0
-        for e in list:
-            for ee in e:
-                s+=ord(ee)
-            break
-        bucket = s % len(table)
-        table[bucket].append(list)
-    return table
-
-
-result = crawl_web('https://www.udacity.com/cs101x/index.html')
-index = add_to_table(result , 10)
-#print(result)
-#print("-----------------------------------------------------")
+index, graph = crawl_web('http://localhost/data')
+ranks = compute_ranks(graph)
+print ranks
 print(lookup(index, 'test'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
